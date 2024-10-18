a1e49b638d1bef43d3e03f4e7ce90fb1ef86dbf3
andikavictory <andika.victory.001@gmail.com>
2024-10-18 11:32:09 +0700

    computer

---
+++ js/pelayananstyle.js
@@ -0,0 +1,49 @@
+
+document.addEventListener('DOMContentLoaded', function(){
+   
+   // memunculkan animasi loading 3 detik untuk menunggu data di load terlebih dahulu
+   const loading = document.querySelector('.loading');
+   document.body.classList.add("no-scroll");
+   
+   // setelah 3 detik, loading dimatikan
+   setTimeout(() => {
+      document.body.classList.remove("no-scroll");
+      loading.style.display ="none";
+   },3000);
+   
+   // get API Spreadsheet
+   const API_KEY = "AIzaSyDv7Vs4gYRG0wgsX_hwiqSU1K5hFd_NY_g";
+   const SHEET_ID= "102qEJ5r79KoA2KRDl85knGxm2au9XDZPDmHq6mW96Tc";
+   const RANGE= "Sheet1!A2:N";
+
+   async function fetchData(){
+      try{
+         const response = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${RANGE}?key=${API_KEY}`)
+         const data = await response.json();
+         
+         const row = data.values;
+         const id = row.map(row => row[0]);
+         const sie = row.map(row => row[1].toUpperCase());
+         const singkatan = row.map(row => row[2]);
+         const hari = row.map(row => row[3]);
+         const waktu = row.map(row => row[4]);
+         const deskripsi = row.map(row => row[5]);
+         const ayat = row.map(row => row[6]);
+         const ketua = row.map(row => row[7]);
+         const sekertaris = row.map(row => row[8]);
+         const bendahara = row.map(row => row[9]);
+         const sieAcara = row.map(row => row[10]);
+         const humas = row.map(row => row[11]);
+         const sieMusik = row.map(row => row[12]);
+         const backgroundHero = row.map(row => row[13]);
+         
+
+      return {id,sie,singkatan,hari,waktu,deskripsi,ayat,ketua,sekertaris,bendahara,sieAcara,humas,sieMusik,backgroundHero}; 
+   } 
+   // menampilkan jika error
+   catch (error) {
+      console.error('error fetching data :', error);
+   }}
+   
+   const data = localStorage.getItem('setEvent');
+})
